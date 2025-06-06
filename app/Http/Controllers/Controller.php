6fc924c6fa6c97b5d000fd91181017a08119d642
garstikaitis <garstikaitis@gmail.com>
2025-06-06 16:35:13 +0300

    feat: add organization and widgets crud

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,21 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected function ok(mixed $data, ?string $message = null)
+    {
+        return response()->json([
+            ...$data,
+            'success' => true,
+            'message' => $message,
+        ]);
+    }
+
+    protected function oops(string $message)
+    {
+        return response()->json([
+            'success' => false,
+            'data' => [],
+            'message' => $message,
+        ]);
+    }
 }
--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -5,8 +5,22 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 final class Organization extends Model
 {
+    use SoftDeletes;
+
     protected $guarded = [];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function widgets(): HasMany
+    {
+        return $this->hasMany(Widget::class);
+    }
 }
